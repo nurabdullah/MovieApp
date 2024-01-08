5cffebeef940331718f0fca90e7fb39cf36336fe
Abdullah NUR <abdullahnur.ybs@gmail.com>
2024-01-09 00:47:02 +0300

    Feat: Created project

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Abdullah Nur on 9.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
